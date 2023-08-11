06825e9acb2d368d87f4d742878dbee6f0242823
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-11 14:37:56 +0430

    additional changes

--- cw17_1/src/main/java/org/example/Main.java
+++ cw17_1/src/main/java/org/example/Main.java
@@ -7,31 +7,13 @@ import org.example.entity.enumaration.TaskPriority;
 import org.example.repository.TaskRepo;
 import org.example.service.TaskService;
 import org.example.service.UserService;
+import org.example.ui.Menu;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User();
-        user1.setUsername("amin");
-        user1.setPassword("aminamin");
-
-        UserService userService = new UserService();
-        userService.save(user1);
-
-
-
-        Task task = new Task();
-        task.setName("nasdasdahar");
-        task.setDescription("emroooooz");
-        task.setDueDate(LocalDate.of(2023, 12, 12));
-        task.setTaskPriority(TaskPriority.HIGH);
-        task.setMode(Mode.COMPLETED);
-        task.setUser(user1);
-
-
-        TaskService taskService = new TaskService();
-        taskService.save(task);
+        Menu.run();
 
 
     }
