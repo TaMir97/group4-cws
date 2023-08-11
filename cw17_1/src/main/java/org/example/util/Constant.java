2e6f7a4635defe32f892a9fa33060c6acf5a91ba
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-11 14:38:18 +0430

    constants to display

---
+++ cw17_1/src/main/java/org/example/util/Constant.java
@@ -0,0 +1,7 @@
+package org.example.util;
+
+public class Constant {
+    public static final String[] ENTRANCE_MENU = {"singIn","signUp","exit"};
+    public static final String[] SORTING_MODE = {"ascending","descending"};
+    public static final String[] SIGN_IN_MENU = {"Display all Tasks","Add new Task","Update Task Mode", "Sort Tasks and Display them", "Exit"};
+}
