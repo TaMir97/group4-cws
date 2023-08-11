6cc870bd52a017fb380af900376aec701362e065
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-11 10:42:48 +0430

    added crud methods to the service with the required transactions

--- cw17_1/src/main/java/org/example/service/TaskService.java
+++ cw17_1/src/main/java/org/example/service/TaskService.java
@@ -11,17 +11,55 @@ public class TaskService {
     private final TaskRepo repository = new TaskRepo();
 
 
-    public void save(Task task){
+    public void save(Task task) {
         try {
             repository.beginTransaction();
             repository.save(task);
 
             repository.commitTransaction();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             repository.rollBack();
         }
 
     }
+
+    public void update(Task task) {
+        try {
+            repository.beginTransaction();
+            repository.update(task);
+            repository.commitTransaction();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            repository.rollBack();
+        }
+    }
+
+    public Task readById(Long id) {
+        try {
+            return repository.readById(id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteById(Long id) {
+        try {
+            repository.beginTransaction();
+            repository.deleteById(id);
+            repository.commitTransaction();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            repository.rollBack();
+        }
+
+    }
+
+    public boolean contains(Task task) {
+        return repository.contains(task);
+
+    }
+
 
 }
