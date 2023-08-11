92827a4f9ef5a01d7d98a83c036d6f061e3c3bcf
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-11 10:43:40 +0430

    added crud methods to the service with the required transactions

--- cw17_1/src/main/java/org/example/service/UserService.java
+++ cw17_1/src/main/java/org/example/service/UserService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 
 import org.example.entity.User;
+import org.example.entity.User;
 import org.example.repository.UserRepo;
 
 public class UserService {
@@ -19,6 +20,43 @@ public class UserService {
         }catch (Exception e){
             repository.rollBack();
         }
+
+    }
+
+    public void update(User user) {
+        try {
+            repository.beginTransaction();
+            repository.update(user);
+            repository.commitTransaction();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            repository.rollBack();
+        }
+    }
+
+    public User readById(Long id) {
+        try {
+            return repository.readById(id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void deleteById(Long id) {
+        try {
+            repository.beginTransaction();
+            repository.deleteById(id);
+            repository.commitTransaction();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            repository.rollBack();
+        }
+
+    }
+
+    public boolean contains(User user) {
+        return repository.contains(user);
 
     }
 
