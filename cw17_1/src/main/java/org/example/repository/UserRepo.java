d4ced4387dbb2874bbb111e90937863d510ba3ac
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-11 10:32:40 +0430

    added crud methods

--- cw17_1/src/main/java/org/example/repository/UserRepo.java
+++ cw17_1/src/main/java/org/example/repository/UserRepo.java
@@ -1,8 +1,8 @@
 package org.example.repository;
 
 import org.example.config.AppEntityManagerFactory;
-import org.example.entity.Task;
 import org.example.entity.User;
+import org.example.exception.UserNotFoundException;
 
 import javax.persistence.EntityManager;
 
@@ -11,6 +11,33 @@ public class UserRepo {
 
     public void save(User user){
         entityManager.persist(user);
+    }
+
+    public void deleteById(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (contains(user))
+            entityManager.remove(user);
+        else
+            throw new UserNotFoundException("The user doesn't exist.");
+    }
+
+    public void update(User user) {
+        if (contains(user))
+            entityManager.merge(user);
+        else
+            throw new UserNotFoundException("The user doesn't exist.");
+    }
+
+    public User readById(Long id) {
+        User user = entityManager.find(User.class, id);
+        if (contains(user))
+            return user;
+        else
+            throw new UserNotFoundException("The user doesn't exist.");
+    }
+
+    public boolean contains(User user) {
+        return user.getId() != null;
     }
 
 
